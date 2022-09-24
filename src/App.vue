edc8752a51a5b886b22099cbed4570e13a40d941
xxWhatOoxx <whato.o@yandex.ru>
2022-09-24 23:53:16 +0300

    123

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
 			height="calc(100vh - 20px)"
 			:current-user-id="currentUserId"
 			:rooms="JSON.stringify(rooms)"
-			:rooms-loaded="true"
+			:rooms-loaded="false"
 			:messages="JSON.stringify(messages)"
 			:messages-loaded="messagesLoaded"
 			@send-message="sendMessage($event.detail[0])"
 			@fetch-messages="fetchMessages($event.detail[0])"
+			@fetch-more-rooms="fetchMoreRooms"
 		/>
 	</div>
 </template>
@@ -22,17 +23,7 @@ export default {
 	data() {
 		return {
 			currentUserId: '1234',
-			rooms: [
-				{
-					roomId: '1',
-					roomName: 'Room 1',
-					avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
-					users: [
-						{ _id: '1234', username: 'John Doe' },
-						{ _id: '4321', username: 'John Snow' }
-					]
-				}
-			],
+			rooms: [],
 			messages: [],
 			messagesLoaded: false
 		}
@@ -94,7 +85,29 @@ export default {
 					}
 				]
 			}, 2000)
+		},
+
+		fetchMoreRooms() {
+			alert("fetched!")
 		}
+	},
+
+	mounted() {
+		let rooms = []
+
+		for (let i = 0; i < 20; i++) {
+			rooms.push({
+				roomId: '1' + i,
+				roomName: 'Room 1' + i,
+				avatar: 'https://66.media.tumblr.com/avatar_c6a8eae4303e_512.pnj',
+				users: [
+					{ _id: '1234', username: 'John Doe' },
+					{ _id: '4321', username: 'John Snow' }
+				]
+			})
+		}
+
+		this.rooms = rooms;
 	}
 }
 </script>
